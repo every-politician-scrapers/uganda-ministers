135b72850d3b3f8b2bc94c48d0ff74f99dda2048
Tony Bowden <tony@tmtm.com>
2022-02-16 11:22:00 +0200

    compare enwiki Bank Governor

---
+++ sources/bank-enwiki/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Name'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name dates].freeze
+    end
+
+    def raw_combo_date
+      super.gsub(/[\(\)]/, '').tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
