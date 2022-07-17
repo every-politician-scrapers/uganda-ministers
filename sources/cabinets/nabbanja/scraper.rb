168297fa393af5b82ed066db5cb6971fb410690a
Tony Bowden <tony@tmtm.com>
2022-07-17 16:30:43 +0300

    compare Nabbanja cabinet

---
+++ sources/cabinets/nabbanja/scraper.rb
@@ -0,0 +1,53 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class MemberList
+  class Members
+    decorator RemoveReferences
+    decorator UnspanAllTables
+    decorator WikidataIdsDecorator::Links
+
+    def member_container
+      noko.xpath("//table[.//th[contains(.,'Ministry')]]//tr[td]")
+    end
+  end
+
+  class Member
+    field :id do
+      name_node.css('a/@wikidata').first
+    end
+
+    field :name do
+      name_node.text.tidy
+    end
+
+    field :positionID do
+    end
+
+    field :position do
+      tds[0].text.split(/\s+and\s+(?=Minister|Leader|Deputy)/).map(&:tidy)
+    end
+
+    field :startDate do
+    end
+
+    field :endDate do
+    end
+
+    private
+
+    def tds
+      noko.css('td')
+    end
+
+    def name_node
+      tds[1]
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url).csv
